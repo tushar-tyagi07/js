46532544262edff598c081c990d2be3c8ede4d1b
tushar-tyagi07 <tt965507@gmail.com>
2026-09-19 12:08:46 +0000

    data types

---
+++ .devcontainer/01_basics/data_types.js
@@ -0,0 +1,23 @@
+"use strict"; //all the code is now treated as newer veersion of js
+
+/*alert("hello");
+alert(3+3);*/ //we are using node.js not browser
+let age=22
+//Data types->
+//number. 2 to the power 52
+//bigint for bigger number
+
+//string => "" or ''
+//boolean => true or false
+
+//null in js it is a standalone value
+
+//undefined mean value is not assigned or it acts as a placeholder for a variable
+//symbol => it is used to find uniqueness 
+
+//object
+
+console.log(typeof null) //object
+console.log(typeof undefined)  //undefined
+
+console.log(typeof age) //number
