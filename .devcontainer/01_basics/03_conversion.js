a725cc672a5882a45b09fa3bb92e770b9d44dbec
tushar-tyagi07 <tt965507@gmail.com>
2026-09-19 16:54:41 +0000

    conversions

---
+++ .devcontainer/01_basics/03_conversion.js
@@ -0,0 +1,18 @@
+let char="33"
+
+let num=Number(char)
+console.log(num)
+/*
+"33"=> 33
+"33a" => NaN
+true => 1 ; false => 0
+undefined => NaN
+null=0
+*/
+
+let guess=1
+let str=Boolean(guess)
+console.log(str);
+
+//null=> false
+//"" => false; "add " or " " => true
