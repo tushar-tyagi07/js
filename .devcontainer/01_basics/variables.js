db11656d9660b6bfaf322d5d4ebe6952ab1cf88b
tushar-tyagi07 <tt965507@gmail.com>
2026-09-19 11:42:54 +0000

    contain config done

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,17 @@
+const accountId=6396
+let accountName="TUSHAR"
+var gamil="xyz@gamil.com"
+accountCity="Newcatel"
+let accountState;
+
+
+//accountId=709929 //not allowed
+accountName="Abcd" 
+gamil="pqrs@gamil.com"
+accountCity="NewYork"
+/*
+prefered not to use var 
+because of issue in block scope and functional block
+*/
+
+console.table([accountId,accountName,accountState,gamil,accountCity]);
