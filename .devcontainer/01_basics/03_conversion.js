2afde14b626344ef897c0eaaa91c963aabba3002
tushar-tyagi07 <tt965507@gmail.com>
2026-09-19 17:34:39 +0000

    conversion

--- .devcontainer/01_basics/03_conversion.js
+++ .devcontainer/01_basics/03_conversion.js
@@ -16,3 +16,15 @@ console.log(str);
 
 //null=> false
 //"" => false; "add " or " " => true
+
+console.log(undefined==0) //false
+console.log(undefined>=0) //false
+console.log(undefined=>0) //[Function (anonymous)]
+
+console.log("2"==2) //true
+console.log("2"===2) //false because it uses strict operator i.e ===
+
+console.log(null==0) //false
+
+console.log(null<0)  //false
+console.log(null<=0)  //true
