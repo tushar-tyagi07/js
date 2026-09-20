9ba5c9b88fd9e9b754be6c2f99b58944c4bd6882
tushar-tyagi07 <tt965507@gmail.com>
2026-09-20 07:49:00 +0000

    string

---
+++ .devcontainer/01_basics/05_variables.js
@@ -0,0 +1,13 @@
+let name="Tushar"
+let instaId="da_tushar_tyagi"
+let upiId="123@ptype"
+//console.log(name+" "+instaId)  outdated
+//use `` -> by using `` concept of sting interpolation comes
+console.log(`My name is ${name} ,my insta handle is ${instaId} and my upi id id ${upiId}`) 
+
+const newstring=new String("TUSHAR TyAgi")
+
+console.log(newstring.length)
+console.log(newstring.charAt("4"))
+console.log(newstring.split("A"))  //split(on the basis of A) return an array 
+console.log(newstring.substring(0,4))
